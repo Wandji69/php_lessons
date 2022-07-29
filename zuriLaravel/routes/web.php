935c8ac3c5c1dd7ba42d48b699218d31edfa8f0d
Wandji69 <collinschuwa@gmail.com>
2022-07-29 18:10:06 +0100

    route to users dasboard

--- zuriLaravel/routes/web.php
+++ zuriLaravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+// User defined path
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('users', App\Http\Controllers\UserController::class);
