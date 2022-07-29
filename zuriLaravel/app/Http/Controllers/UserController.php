72b8e054566079571cbb423c2429fbd9a500ac4f
Wandji69 <collinschuwa@gmail.com>
2022-07-29 17:50:15 +0100

    adding assert files controllers

---
+++ zuriLaravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::select('id', 'name', 'phone_number', 'email')->get();
+        return view('users.list')->with([
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd($request->all());
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+        try {
+            DB::beginTransaction();
+            // Logic For Save User Data
+
+            $create_user = User::create([
+                'name' => $request->name,
+                'phone_number' => $request->phone_number,
+                'email' => $request->email,
+            ]);
+
+            if (!$create_user) {
+                DB::rollBack();
+
+                return back()->with('error', 'Something went wrong while saving user data');
+            }
+
+            DB::commit();
+            return redirect()->route('users.index')->with('success', 'User Stored Successfully.');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $users = User::select('id', 'name', 'phone', 'email')->get();
+
+        return view('users.list')->with([
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user =  User::whereId($id)->first();
+
+        if (!$user) {
+            return back()->with('error', 'User Not Found');
+        }
+
+        return view('users.edit')->with([
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        try {
+            DB::beginTransaction();
+            // Logic For Save User Data
+
+            $update_user = User::where('id', $id)->update([
+                'name' => $request->name,
+                'phone' => $request->phone,
+                'email' => $request->email
+            ]);
+
+            if (!$update_user) {
+                DB::rollBack();
+
+                return back()->with('error', 'Something went wrong while update user data');
+            }
+
+            DB::commit();
+            return redirect()->route('users.index')->with('success', 'User Updated Successfully.');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            DB::beginTransaction();
+
+            $delete_user = User::whereId($id)->delete();
+
+            if (!$delete_user) {
+                DB::rollBack();
+                return back()->with('error', 'There is an error while deleting user.');
+            }
+
+            DB::commit();
+            return redirect()->route('users.index')->with('success', 'User Deleted successfully.');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+}
